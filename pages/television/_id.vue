7e4f3290e4aa2df791dd0f74abb5a4f448d66ef1
Serge M <serge@tommyjohn.com>
2021-04-19 00:22:31 -0400

    allow for televisions show to render correctly

--- pages/television/_id.vue
+++ pages/television/_id.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <h1>Television</h1>
+    <img :src="require(`~/assets/img/television/${show.image}`)" :alt="show.title">
+    <h1>{{show.title}}</h1>
+
+    <p><strong>Role:</strong> {{ show.role }}</p>
+    <p>{{ show.content}}</p>
+    <nuxt-link to="/work" class="return">Back to Filmography</nuxt-link>
   </div>
 </template>
 
 <script>
+  import tv from "~/data/televison";
   export default {
-
+    data(){
+      return {
+        show: {}
+      }
+    },
+    created(){
+      const show = tv.shows.find((show) => show.id == this.$route.params.id);
+      this.show = show;
+    }
   }
 </script>
 
